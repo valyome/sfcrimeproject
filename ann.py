22d7569ea3662396b4b372dbb1c060d2599e11d8
Jeff Cui <jeffcui@minerva.kgi.edu>
2016-04-22 10:33:22 -0700

    started ann

--- ann.py
+++ ann.py
@@ -2,47 +2,57 @@ import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
 from sklearn.metrics import log_loss
-from sklearn import naive_bayes
-from sklearn.linear_model import LogisticRegression
 import numpy as np
+from pybrain.datasets import ClassificationDataSet
+
+CATEGORIES = [  'WARRANTS',                     'OTHER OFFENSES',               'LARCENY/THEFT',            'VEHICLE THEFT',        # 0 -  3
+                'VANDALISM',                    'NON-CRIMINAL',                 'ROBBERY',                  'ASSAULT',              # 4 -  7
+                'WEAPON LAWS',                  'BURGLARY',                     'SUSPICIOUS OCC',           'DRUNKENNESS',          # 8 - 11
+                'FORGERY/COUNTERFEITING',       'DRUG/NARCOTIC',                'STOLEN PROPERTY',          'SECONDARY CODES',      #12 - 15
+                'TRESPASS',                     'MISSING PERSON',               'FRAUD',                    'KIDNAPPING',           #16 - 19
+                'RUNAWAY',                      'DRIVING UNDER THE INFLUENCE',  'SEX OFFENSES FORCIBLE',    'PROSTITUTION',         #20 - 23
+                'DISORDERLY CONDUCT',           'ARSON',                        'FAMILY OFFENSES',          'LIQUOR LAWS',          #24 - 27
+                'BRIBERY',                      'EMBEZZLEMENT',                 'SUICIDE',                  'LOITERING',            #28 - 31
+                'SEX OFFENSES NON FORCIBLE',    'EXTORTION',                    'GAMBLING',                 'BAD CHECKS',           #32 - 35
+                'TREA',                         'RECOVERED VEHICLE',            'PORNOGRAPHY/OBSCENE MAT']                          #36 - 38
 
 #Load Data with pandas, and parse the first column into datetime
 
-train=pd.read_csv('train.csv', parse_dates = ['Dates'],low_memory=False)
-test=pd.read_csv('test.csv', parse_dates = ['Dates'],low_memory=False)
+train_csv = pd.read_csv('data/train.csv', parse_dates = ['Dates'],low_memory=False)
+test_csv = pd.read_csv('data/test.csv', parse_dates = ['Dates'],low_memory=False)
 
+def normalize(data_list):
+	result = (data_list - data_list.mean()) / (data_list.max() - data_list.min())
+	return result
 
-#Convert crime labels to numbers
+# restructures data
 le = preprocessing.LabelEncoder()
-crime = le.fit_transform(train.Category)
-day = pd.DataFrame(le.fit_transform(train.DayOfWeek))
-district = pd.DataFrame(le.fit_transform(train.PdDistrict))
-hour = train.Dates.dt.hour
-train_data = pd.concat([hour, day, district], axis=1)
-train_data['crime']=crime
-train_data.columns = ['hour','day', 'PdDistrict','crime']
-train_data[:5]
+
+crime = le.fit_transform(train_csv.Category) # numbered crime categories
+day = pd.DataFrame(le.fit_transform(train_csv.DayOfWeek))
+district = pd.DataFrame(le.fit_transform(train_csv.PdDistrict))
+hour = train_csv.Dates.dt.hour
+location_x = normalize(train_csv["X"])
+location_y = normalize(train_csv["Y"])
+
+features = pd.concat((hour, day, district, location_x, location_y), axis=1)
+features.columns = ['Hour', 'Day', 'PdDistrict', 'X', 'Y']	# renaming the columns
+
+classify = ClassificationDataSet(5, class_labels = CATEGORIES)
+
+for i in range(len(crime)):
+        classify.appendLinked([hour[i], day[i], district[i], location_x[i], location_y[i]], [crime[i]])
 
 
-training, validation = train_test_split(train_data, train_size=.60,random_state=0)
-print type(training), type(validation)
-features = ['hour','day','PdDistrict']
+validation, train = classify.splitWithProportion(0.25)
+validation._convertToOneOfMany()
+train._convertToOneOfMany()
 
-naivebayesianmodel = naive_bayes.BernoulliNB()
-naivebayesianmodel.fit(training[features], training['crime'])
-predicted = np.array(naivebayesianmodel.predict_proba(validation[features]))
+print classify
+
+# naivebayesianmodel = naive_bayes.BernoulliNB()
+# naivebayesianmodel.fit(training[features], training['crime'])
+# predicted = np.array(naivebayesianmodel.predict_proba(validation[features]))
 
 print log_loss(validation['crime'], predicted)
 
-
-from sklearn import tree
-training1, validation1 = train_test_split(train_data, train_size=.90,random_state=0)
-decisiontree = tree.DecisionTreeClassifier(max_depth = 5)
-decisiontree.fit(training1[features], training1['crime'])
-predictedtree = np.array(decisiontree.predict_proba(validation1[features]))
-
-print log_loss(validation1['crime'], predictedtree)
-
-
-print training1
-print validation1
